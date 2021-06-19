849058d82fffe6fa9f956f550efd41eef5aa6bd5
Efosa Igbinehi <liova2273@gmail.com>
2021-06-19 23:18:52 +0100

    added a 404 page

---
+++ 404.php
@@ -0,0 +1,13 @@
+<?php get_header(); ?>
+
+<section class="page-not-found">
+    <div class="container text-center">
+        <h1>404</h1>
+        <h3>Page not found!</h3>
+        <p>The page you're looking for has either moved or does not exist.</p>
+        <a href="/" class="btn-purple">Go back home</a>
+        <a href="<?php get_site_url(); ?>/connect-with-us" class="btn-red">Connect with us</a>
+    </div>
+</section>
+
+<?php get_footer(); ?>
